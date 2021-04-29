d7082d603bffa592f995bac90a9828e94350f57f
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-04-29 14:04:50 +0200

    zadanie-hooks

--- src/Form.js
+++ src/Form.js
@@ -1,41 +1,55 @@
-import React, {useState} from 'react';
-import './Form.css';
+import React, { useState } from "react";
+import "./Form.css";
 
-function Form(props) {
+function Form() {
+  const [inputImie, setInputImie] = useState("");
+  const [inputNazwisko, setInputNazwisko] = useState("");
+  const [textToShow, setTextToShow] = useState("");
+  const [showError, setShowError] = useState("");
 
-    const [inputImie, setInputImie] = useState('h1');
-    const [textsubmit , setTextSubmit] = useState(props.initValue);
-    const [inputImie , setInputImie] = useState('');
+  const handleInputImieChange = (event) => {
+    setInputImie(event.target.value);
+  };
 
-    const handleInputImieChange = (event) => {
-        setInputImie(event.target.value)
+  const handleInputNazwiskoChange = (event) => {
+    setInputNazwisko(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setShowError("");
+
+    if (inputImie && inputNazwisko) {
+      setTextToShow(`${inputImie} ${inputNazwisko}`);
+    } else if (!inputImie && !inputNazwisko) {
+      setShowError("Uzupełnij imię i nazwisko");
+    } else if (!inputImie) {
+      setShowError("Uzupełnij imię");
+    } else {
+      setShowError("Uzupełnij nazwisko");
     }
-       
-    const formSubmit = (event) => {
-        event.preventDefault();
-    }
+  };
 
-    const changeSubmit = () => {
-        setTextSubmit((prevSubmit)=> {
-            return prevSubmit + textsubmit;
-        });
-    }
-    return (
-        <div className="color-main">
-            <h1 className="text" value={setInputImie}></h1>
-
-            <form onSubmit={setTextSubmit}>
-                Imię : <br />
-                <input type="text" value={inputImie} onChange={handleInputImieChange} /> <br />
-
-                Nazwisko : <br />
-                <input type="text" /> <br /><br />
-
-                
-                <button type="submit" onSubmit={textsubmit}>Wyświetl Dane</button> 
-            </form>
-        </div>
-    )
+  return (
+    <div className="color-main">
+      <h1 className="text">{textToShow}</h1>
+      <form onSubmit={onSubmit}>
+        Imię : <br />
+        <input type="text" value={inputImie} onChange={handleInputImieChange} />
+        <br />
+        Nazwisko : <br />
+        <input
+          type="text"
+          value={inputNazwisko}
+          onChange={handleInputNazwiskoChange}
+        />
+        <br />
+        <br />
+        <button type="submit">Wyświetl Dane</button>
+      </form>
+      {showError}
+    </div>
+  );
 }
 
 export default Form;
